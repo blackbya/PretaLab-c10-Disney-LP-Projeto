6df443c36638cffe3d44b3725717f491bfe95949
Beatrys Rodrigues da Silva <beatrys1707@gmail.com>
2024-06-02 14:45:06 -0300

    funcionalidade de dicas e vitórias

--- src/script.js
+++ src/script.js
@@ -1,25 +1,27 @@
-let numeroAleatorio = 0;
+let numeroAleatorio = 68;
 let tentativas = 0;
 
 function jogoDeAdivinhacao() {
-    /*
-    Guiado
-    ---
-    1. Não aceita palpite vazio
-    2. Dar dicas a cada palpite (maior ou menor)
-    3. Atualizar a pontuação a cada palpite errado
-    4. Mostrar todos os palpites errados
-    5. Deve-se poder reiniciar o jogo a qualquer momento
-    6. Se a pontuação chegar a zero, deve ser exibido um alerta e reiniciado o jogo
-    
-    Individual
-    ---
-    7. Só deve aceitar numeros entre 1 e 100
-    8. Não deve aceitar palpite repetido
-    */
+   const palpiteDigitado = pegarPalpiteDigitado();
+
+   if (!palpiteDigitado) {
+    alert('Digite um valor válido')
+   }
+
+   if (palpiteDigitado === numeroAleatorio) {
+    alert('Parabéns, você adivinhou!')
+   } else if(palpiteDigitado > numeroAleatorio) {
+    tentativas++;
+    atualizarFeedback('O número é muito alto. Tente novamente.');
+   } else {
+    tentativas++;
+    atualizarFeedback('O número é muito baixo. Tente novamente.');
+
+    const novaPontuacao = 100 - (tentativas * 10);
+    atualizarPontuacao(novaPontuacao);
+}
 
 
-    // to-do
 }
 
 function reiniciarJogo() {
